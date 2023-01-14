b29132850d8e92d38960ea4abbbf611405350f2d
Sheena Sang <sheenasany@gmail.com>
2023-01-13 21:30:47 -0500

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    if length > 0:
+        fibonacci_sequence.append(0)
+        if length > 1: 
+            fibonacci_sequence.append(1)
+        if length > 2: 
+            for index in range(2, length):
+                fibonacci_sequence.append(
+                    fibonacci_sequence[index - 1] + fibonacci_sequence[index - 2])
+                        #taking the last element of the index and adding second to last element of the index
+    print(fibonacci_sequence)
+            
